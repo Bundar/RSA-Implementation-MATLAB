631e04d7272dcf5872dedb610694add8a8f22d6b
Bundar <birnie.nick@gmail.com>
2019-10-20 21:06:03 -0400

    Converted to proper matlab code

--- AllCode.m
+++ AllCode.m
@@ -1,5 +1,5 @@
-## Project CP 1 NetSec
-## Dunbar B and Phillip J
+%% Project CP 1 NetSec
+%% Dunbar B and Phillip J
 
 function AllCode ()
   keys = getKeys();
@@ -7,18 +7,18 @@ function AllCode ()
   publicKey = keys(1, 1:2);
   privateKey = keys(2, 1:2);
   cipherText = encryption(publicKey, plainText);
-  printf("The encrypted Message is: ");
+  fprintf("The encrypted Message is: \n");
   for i = 1:length(cipherText)
-    printf("%i ", cipherText(i));
-  endfor
+    fprintf("%i ", cipherText(i));
+  end
   select = input("\nWould you like to decrypt back? \n", 's');
-  if (select == 'yes')
+  if select == "yes"
     pt = decryption(privateKey, cipherText);
-    printf("The decrypted cipher text is: %s\n", char(pt));
-  endif
-endfunction
+    fprintf("The decrypted cipher text is: %s\n", char(pt));
+  end
+end
 
-## Returns Public and Private Keys
+%% Returns Public and Private Keys
 function keys = getKeys()
   pq = findTwoPrimes();
   p = pq(1);
@@ -30,104 +30,105 @@ function keys = getKeys()
   publicKey = [e, n];
   privateKey = [d, n];
   keys = [publicKey; privateKey];
-endfunction
+end
 
-## Encrypts the given plain text using the generated public key.
+%% Encrypts the given plain text using the generated public key.
 function cipherArr = encryption ( publicKey, plainText)
   pt = double(plainText);
   e = publicKey(1);
   n = publicKey(2);
+  cipherArr = zeros(length(plainText));
   for i = 1:length(pt)
     m = pt(i);
     cipherArr(i) = powerMod(m, e, n);
-  endfor
-endfunction
+  end
+end
 
-## Decrypts the given cipher text using the generated private key.
+%% Decrypts the given cipher text using the generated private key.
 function plainText = decryption ( privateKey, cipherArr)
   d = privateKey(1);
   n = privateKey(2);
+  plainText = zeros(length(cipherArr));
   for i = 1:length(cipherArr)
     m = cipherArr(i);
     plainText(i) = powerMod(m, d, n);
-  endfor
-endfunction
+  end
+end
 
-## Computes encrypt and decrypt exponents for public and private key gen.
+%% Computes encrypt and decrypt exponents for public and private key gen.
 function retval = computeEandD(p , q)
-  n = p*q;
   totient = (p-1)*(q-1);
   e = 0;
   for k = 2:totient
     if(gcd(k, totient) == 1)
       e = k;
       break;
-    endif
-  endfor
+    end
+  end
   
   d = extendedEuclidianAlgorithm(e, totient);
   retval = [e, d];
-endfunction
+end
 
-## used to solve inverse mod problem to get d.
+%% used to solve inverse mod problem to get d.
 function inv = extendedEuclidianAlgorithm(e, totient)
-  [G, A, B] = gcd(totient, e);
+  [~, ~, B] = gcd(totient, e);
   inv = totient + B;
   assert(mod(e*inv, totient) == 1);
-  #printf("mod(e*inv, totient) == %i\n", mod(e*inv, totient));
-endfunction
+  %printf("mod(e*inv, totient) == %i\n", mod(e*inv, totient));
+end
 
-## Code to generate two random primes using miller rabin method.
+%% Code to generate two random primes using miller rabin method.
 function twoPrimes = findTwoPrimes()
   i = 1;
   twoPrimes = [0, 0];
   while(i < 3)
     p = floor(rand()*10000);
     if(myMillerRabin(p, 6) == 1)
-      #p is a prime
+      %p is a prime
       twoPrimes(i) = p;
       i = i + 1;
-    endif
-  endwhile
-endfunction
+    end
+  end
+end
 
-## Miller Rabin code to check if a number is prime... probably.
+%% Miller Rabin code to check if a number is prime... probably.
 function retval = myMillerRabin (n, k)
     if (n <= 2)
         retval = -1;
         return;
-    endif
+    end
     d = n - 1;
     s = 0;
     while(mod(d,2)==0)
         d = d / 2;
         s = s + 1;
-    endwhile
-    assert((2**s)*d == n-1);
+    end
+    assert((2^s)*d == n-1);
     for i = 1:k
         a = 2 + floor(rand*(n-3));
         x = powerMod(a, d, n); 
         if(x == 1 || x == n-1)
             retval = 1;
             return;
-        endif
-        while (d != n-1)
+        end
+        while (d ~= n-1)
             x = mod( x*x, n );
             d = d * 2;
             if(x == 1)
               retval = 0;
               return;
-            endif
+            end
             if(x == n-1)
               retval = 1;
               return;
-            endif
-        endwhile 
-    endfor
+            end
+        end
+    end
     retval = 0;
-endfunction
+end
 
-## power mod function calculates powers of a base mod n so that it doesnt over flow the double precision.
+%% power mod function calculates powers of a base mod n so that it doesnt over flow the double precision.
 function ret = powerMod(x, y, p)
   ret = 1;
   x = mod(x, p);
@@ -135,9 +136,9 @@ function ret = powerMod(x, y, p)
     if(mod(y, 2) == 1)
       ret = mod(ret * x,p);
       y = y - 1;
-    endif
+    end
     y = y / 2;
     x = mod((x*x),p);
-  endwhile 
-endfunction
+  end
+end
 
